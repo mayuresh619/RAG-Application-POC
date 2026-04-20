eab7b8e9b87e2ea542ab1d59df4cbcf3ef299b89
mayuresh619 <mayuresh.vedak619@gmail.com>
2026-04-20 13:20:14 +0530

    Added pg vector db for embeddings

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿using DocumentFormat.OpenXml.Office.SpreadSheetML.Y2023.MsForms;
-using Microsoft.KernelMemory;
+﻿using Microsoft.KernelMemory;
 using Microsoft.KernelMemory.AI;
 using Microsoft.KernelMemory.AI.Ollama;
+using Microsoft.KernelMemory.DocumentStorage.DevTools;
+using Microsoft.KernelMemory.FileSystem.DevTools;
 using System.Text;
 
 var ollamaConfig = new OllamaConfig
@@ -11,16 +12,36 @@ var ollamaConfig = new OllamaConfig
     EmbeddingModel = new OllamaModelConfig("nomic-embed-text:latest", 2048) // Your embedding model
 };
 
+var postgresConfig = new PostgresConfig
+{
+    ConnectionString = "Host=localhost;Port=5432;Database=ragdb;Username=postgres;Password=123456789",
+    Schema = "public",
+};
+
 var chat = new OllamaTextGenerator(ollamaConfig);
 
 var memory = new KernelMemoryBuilder()
     .WithOllamaTextGeneration(ollamaConfig)
     .WithOllamaTextEmbeddingGeneration(ollamaConfig)
+    .WithPostgresMemoryDb(postgresConfig)
+    .WithSimpleFileStorage(new SimpleFileStorageConfig
+    {
+        StorageType = FileSystemTypes.Disk,
+        Directory = "km-cache"
+    })
     .Build();
 
-await memory.ImportDocumentAsync(Path.GetFullPath("Society_Rules_Sample.pdf"));
+string docid = await memory.ImportDocumentAsync(Path.GetFullPath("Society_Rules_Sample.pdf"));
+
+while (!await memory.IsDocumentReadyAsync(docid))
+{
+Console.WriteLine("Still indexing...");
+await Task.Delay(1000);
+}
+Console.WriteLine("Document ready!");
+
 GoHere:
-Console.WriteLine("Please choose mode:");
+    Console.WriteLine("Please choose mode:");
 Console.WriteLine("1. From Document");
 Console.WriteLine("2. For LLM");
 var mode = Console.ReadLine();
